7961f7b4b41a1e6172c18a76bb20364b9a4bdd95
ShemKebabe <ogendishem1@gmail.com>
2026-03-09 13:59:23 +0300

    Login update

--- app/src/main/java/com/example/verify/LoginActivity.java
+++ app/src/main/java/com/example/verify/LoginActivity.java
@@ -1,34 +1,38 @@
 package com.example.verify;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-public class MainActivity extends AppCompatActivity {
+
+public class LoginActivity extends AppCompatActivity {
+
     EditText username;
     EditText password;
     Button loginButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         username = findViewById(R.id.username);
-        password = findViewById(R.id.password_);
+        password = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
-                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-    }
-
-    private EditText findViewById(int loginButton) {
-        return null;
     }
 }
